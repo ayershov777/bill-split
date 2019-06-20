3c343ce40e53d2a737cbeccbc78afc64c5690e26
Anton Yershov <anthony.yershov@gmail.com>
2019-06-19 22:57:55 -0700

    finished split and payment

--- main_app/forms.py
+++ main_app/forms.py
@@ -11,8 +11,3 @@ class GroupCreateForm(ModelForm):
     class Meta:
         model = Group
         fields = ['title', 'users']
-
-class AddUserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username']
